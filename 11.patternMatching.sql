a2bc6ebc4c3b11a235da7f272575a1d75b1a77ff
000deepak <90568067+000deepak@users.noreply.github.com>
2026-04-23 18:50:59 +0530

    Update 11.patternMatching.sql

--- 11.patternMatching.sql
+++ 11.patternMatching.sql
@@ -1,33 +1,36 @@
 select * from superstore_orders
 
-#start with A
+-- start with A
 select Customer_Name from superstore_orders
 where Customer_Name like 'A%';
-#end with A
+
+-- end with A
 select Customer_Name from superstore_orders
 where Customer_Name like '%A';
 
-#length with 3 char end last two char end with an,_specify single char
+-- length with 3 char end last two char end with an,_specify single char
 select Customer_Name from superstore_orders
 where Customer_Name like '_an';
 
-#start with either a or c or d,SIGLE CHAR CONSTRAINT
+-- start with either a or c or d,SIGLE CHAR CONSTRAINT
 select Customer_Name from superstore_orders
 where Customer_Name like '[ACD]%';
 
-#start with RANGE
+-- start with RANGE
 select Product_Name from superstore_orders
 where Product_Name like '[A-P]%';
-#NOT BETWEEN A AND P
+
+-- NOT BETWEEN A AND P
 select Product_Name from superstore_orders
 where Product_Name like '[^A-P]%';
 
 select ORDER_ID from superstore_orders;
-#3RD CHAR WOULD BE HIPHEN
+
+-- 3RD CHAR WOULD BE HIPHEN
 select ORDER_ID from
  superstore_orders WHERE ORDER_ID LIKE '__-%';
  
- #
+ -- 
  SELECT PRODUCT_NAME from
  superstore_orders
  WHERE PRODUCT_NAME LIKE '%10[/]%'; 

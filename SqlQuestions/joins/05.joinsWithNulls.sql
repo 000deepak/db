e659624c1dc02ece2fc1fd2d24263460c1e86aef
000deepak <90568067+000deepak@users.noreply.github.com>
2026-04-24 17:13:32 +0530

    Update 05.joinsWithNulls.sql

--- SqlQuestions/joins/05.joinsWithNulls.sql
+++ SqlQuestions/joins/05.joinsWithNulls.sql
@@ -1,56 +1,37 @@
-USE dbo;
-GO
-
 -- Create tables
-CREATE TABLE tableA (
-                        id INT
-);
-
-CREATE TABLE tableB (
-                        id INT
-);
+CREATE TABLE tableA (id INT);
+CREATE TABLE tableB (id INT);
 
 -- View initial data
 SELECT * FROM tableA;
 
 -- Insert data
-INSERT INTO tableA (id)
-VALUES (NULL);
+INSERT INTO tableA (id) VALUES (NULL);
+INSERT INTO tableB (id) VALUES (NULL);
 
-SELECT * FROM tableB;
-
-INSERT INTO tableB (id)
-VALUES (NULL);
-
-/*
-Expected number of rows:
-1. INNER JOIN  = 11
-2. LEFT JOIN   = 10
-3. FULL OUTER  = (depends on data)
-*/
 
 -- LEFT JOIN
 SELECT *
 FROM tableA AS A
          LEFT JOIN tableB AS B
-                   ON A.id = B.id;  -- ~10 rows
+                   ON A.id = B.id; 
 
 -- RIGHT JOIN
 SELECT *
 FROM tableA AS A
          RIGHT JOIN tableB AS B
-                    ON A.id = B.id;  -- ~11 rows
+                    ON A.id = B.id; 
 
 -- INNER JOIN
 SELECT *
 FROM tableA AS A
          INNER JOIN tableB AS B
-                    ON A.id = B.id;  -- ~8 rows
+                    ON A.id = B.id; 
 
 -- CROSS JOIN (note: no ON clause needed)
 SELECT *
 FROM tableA AS A
-         CROSS JOIN tableB AS B;  -- Cartesian product
+         CROSS JOIN tableB AS B; 
 
 /*
 MySQL does not support FULL OUTER JOIN directly,
